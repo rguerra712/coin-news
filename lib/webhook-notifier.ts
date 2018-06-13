c4421618512c13c3c8a08f310030d08de5cb3369
stephen <rguerra712@yahoo.com>
2018-06-12 23:16:23 -0500

    Added a quick trigger for notifications to trigger notifications with IFTTT

---
+++ lib/webhook-notifier.ts
@@ -0,0 +1,10 @@
+import { Sender, Message } from 'node-gcm';
+import request from 'axios';
+
+export default class WebhookNotifier {
+    trigger():void {
+        request(process.env["WEBHOOK_URL"] || '')
+            .then(data => console.log(`Coin news webhook triggered: ${data}`))
+            .catch(error => console.error(`Coin News webhook error: ${error}`));
+    }
+}
